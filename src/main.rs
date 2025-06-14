9975af1122cc793fe2fa103910c0f2e434353d0b
Oliver Emrich <Oliver.Emrich@posteo.de>
2025-06-14 22:33:51 +0200

    Add default window and hello world update function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use bevy::prelude::*;
+
+
+fn setup(
+) {
+
+}
+
+fn hello_world(
+) {
+    for n in 1..101 {
+        if n % 15 == 0 {
+            println!("Hello, world {}!", n);
+        }
+    }
+}
 fn main() {
-    println!("Hello, world!!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, hello_world)
+        .run();
 }
